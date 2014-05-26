73b85b7237cbb34ad67ff47833b3c0ba88b2d35a
tagir <tagirr1@yandex.ru>
2014-05-26 21:39:39 +0400

    added project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guild::Application.config.secret_token = '7e8b4c41f4bfa5c48ff5ef352de2cbd3809be711aff2640a5f0dc7ff8da8ed8b4f1bd1ad5dc495d534504a4f8beb209ba0641f0fd4ae97ef07a7581db5367858'
